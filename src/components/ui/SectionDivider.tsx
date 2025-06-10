eacb80274ae365bbbf9707133968aa84a7f57192
Chiamaka <lillyannmaka@gmail.com>
2025-06-10 18:29:38 +0100

    feat: translated how it works section and created a section divider component

---
+++ src/components/ui/SectionDivider.tsx
@@ -0,0 +1,13 @@
+const SectionDivider = () => {
+  return (
+    <div className="relative my-8">
+      <div className="h-px bg-light-background/30 w-full" />
+      {/* 2. Centered “drop” diamond */}
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="size-3.5 bg-accent-button rotate-45" />
+      </div>
+    </div>
+  );
+};
+
+export default SectionDivider;
