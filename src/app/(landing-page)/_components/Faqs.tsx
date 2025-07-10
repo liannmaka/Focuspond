84d2750da8b35ffaab0400995e12380010836c4a
Chiamaka <lillyannmaka@gmail.com>
2025-07-10 14:12:47 +0100

    chore: added svg shape to faq section

--- src/app/(landing-page)/_components/Faqs.tsx
+++ src/app/(landing-page)/_components/Faqs.tsx
@@ -1,25 +1,3 @@
-// import Badge from "@/components/ui/Badge";
-
-// const Faqs = () => {
-//   return (
-//     <section className="bg-[#a8c686]/10 py-10">
-//       <div className="container mx-auto px-7">
-//         <Badge title="FAQs" />
-//         <div className="text-center">
-//           <h2 className="text-2xl md:text-3xl lg:4xl font-sora font-semibold">
-//             Frequently asked questions
-//           </h2>
-//           <p className="mt-4 font-manrope text-base/7 text-walnut-brown/90 md:text-lg/7">
-//             Check out the most common questions
-//           </p>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Faqs;
-
 import Badge from "@/components/ui/Badge";
 
 const Faqs = () => {
@@ -28,13 +6,21 @@ const Faqs = () => {
       {/* Top curve */}
       <div className="absolute -top-1 w-full overflow-hidden leading-none">
         <svg
-          viewBox="0 0 1440 100"
+          viewBox="0 0 1440 160"
           xmlns="http://www.w3.org/2000/svg"
           className="block w-full"
         >
           <path
             fill="#fdf9f3"
-            d="M0,0 C480,100 960,0 1440,100 L1440,0 L0,0 Z"
+            d="
+         M0,64 
+          C120,120 360,40 540,80 
+          C720,120 960,160 1140,100 
+         C1320,40 1440,80 1440,80 
+        L1440,0 
+      L0,0 
+      Z
+    "
           />
         </svg>
       </div>
