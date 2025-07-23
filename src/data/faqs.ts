f8f6c04d770d27309664bb15b96f07ea518a6a1d
Chiamaka <lillyannmaka@gmail.com>
2025-07-23 23:50:56 +0100

    chore: updated faq section

---
+++ src/data/faqs.ts
@@ -0,0 +1,37 @@
+export const faqs = [
+  {
+    question: "What is Focuspond?",
+    answer:
+      "Focuspond is your quiet corner of the internet to plan, focus, and feel less overwhelmed. It's a calm productivity space for deep thinkers, with a Pomodoro timer, daily tasks, frog picking, and mood check-ins, all saved on your device with no account needed.",
+  },
+  {
+    question: "Is Focuspond free?",
+    answer:
+      "Yes, Focuspond is completely free while we’re in our early stage. You’ll always have access to the basics, even when premium features arrive later.",
+  },
+  {
+    question: "Does it work offline?",
+    answer:
+      "Absolutely. Focuspond is built as a PWA, so you can plan and focus even without data or Wi-Fi.",
+  },
+  {
+    question: "What is the 'Frog of the Day'?",
+    answer:
+      "Your Frog of the Day is the most important task you choose to tackle first. It’s not necessarily urgent, just the thing that’ll make you feel accomplished once it’s done.",
+  },
+  {
+    question: "How do I identify a 'Frog task'?",
+    answer:
+      "A Frog task is the one you’re most likely to avoid, usually because it feels big, challenging, or meaningful. Ideally, it should take 1 to 4 hours. If it feels overwhelming, break it into smaller steps.",
+  },
+  {
+    question: "Can I use Focuspond without creating an account?",
+    answer:
+      "Yes, you can use Focuspond without signing up. But if you want to back up your data or use it on multiple devices, you can create an account anytime.",
+  },
+  {
+    question: "What is the vibe switcher?",
+    answer:
+      "It lets you choose how the app 'talks' to you, formal, friendly, or full-on pidgin. Change the tone to match your mood or mindset.",
+  },
+];
