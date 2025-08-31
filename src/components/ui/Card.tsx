cca76c7a94d69e42177e51119522e2d63f43d32a
Chiamaka <lillyannmaka@gmail.com>
2025-08-31 11:32:57 +0100

    chore: updated the card component

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,33 +1,32 @@
 import clsx from "clsx";
+import { forwardRef } from "react";
 
-type CardProps = React.PropsWithChildren<
-  React.HTMLAttributes<HTMLDivElement>
-> & {
-  shadow?: boolean;
-  bordered?: boolean;
-  rounded?: boolean;
+type CardProps = React.HTMLAttributes<HTMLDivElement> & {
+  shadow?: "none" | "sm" | "md" | "lg";
+  rounded?: "none" | "2xl" | "3xl";
+  children: React.ReactNode;
+  className?: string;
 };
 
-const Card = ({
-  children,
-  shadow = true,
-  bordered = false,
-  rounded = true,
-  ...rest
-}: CardProps) => {
-  return (
-    <div
-      className={clsx(
-        "p-4 transition duration-300",
-        shadow && "shadow-md",
-        bordered && "border border-gray-200",
-        rounded && "rounded-2xl"
-      )}
-      {...rest}
-    >
-      {children}
-    </div>
-  );
-};
+const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ shadow = "sm", rounded = "2xl", children, className, ...rest }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={clsx(
+          "p-4 transition duration-200",
+          shadow !== "none" && `shadow-${shadow}`,
+          rounded !== "none" && `rounded-${rounded}`,
+          className
+        )}
+        {...rest}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+Card.displayName = "Card";
 
 export default Card;
