9d24a9dceab7743f22d0a2f3642481deccf1230b
Chiamaka <lillyannmaka@gmail.com>
2025-09-26 15:50:02 +0100

    chore: updated faq item component

--- src/app/(landing-page)/_components/faq/FaqItem.tsx
+++ src/app/(landing-page)/_components/faq/FaqItem.tsx
@@ -9,42 +9,45 @@ type FaqItemProp = {
   onClick: () => void;
 };
 
-const FaqItem = memo(({ index, question, answer, isOpen, onClick }: FaqItemProp) => {
-  const contentRef = useRef<HTMLDivElement>(null);
+const FaqItem = memo(
+  ({ index, question, answer, isOpen, onClick }: FaqItemProp) => {
+    const contentRef = useRef<HTMLDivElement>(null);
 
-  return (
-    <div className="border-b border-dark-accent/20 pb-2.5 last:border-b-0">
-      <button
-        aria-expanded={isOpen}
-        aria-controls={`faq-content-${index}`}
-        onClick={onClick}
-        className="flex justify-between items-center w-full text-left px-4 pb-4 font-medium font-sora cursor-pointer text-[#5a3a24]"
-      >
-        <span className="pr-2">{question}</span>
-        <ChevronDown
-          className={`flex-shrink-0 transition-transform duration-300 ${isOpen ? "rotate-180" : ""
+    return (
+      <div className="border-b border-dark-accent/20 pb-2.5 lg:last:border-b-0">
+        <button
+          aria-expanded={isOpen}
+          aria-controls={`faq-content-${index}`}
+          onClick={onClick}
+          className="flex justify-between items-center w-full text-left px-4 pb-4 font-medium font-sora cursor-pointer text-[#5a3a24]"
+        >
+          <span className="pr-2">{question}</span>
+          <ChevronDown
+            className={`flex-shrink-0 transition-transform duration-300 ${
+              isOpen ? "rotate-180" : ""
             }`}
-        />
-      </button>
+          />
+        </button>
 
-      <div
-        ref={contentRef}
-        className="px-4 overflow-hidden transition-all duration-500 ease-in-out text-[#5a3a24]"
-        id={`faq-content-${index}`}
-        role="region"
-        aria-labelledby={`faq-header-${index}`}
-        style={{
-          maxHeight: isOpen ? contentRef.current?.scrollHeight : 0,
-          opacity: isOpen ? 1 : 0,
-        }}
-      >
-        <p className="py-2 text-sm text-walnut-brown/80 font-manrope ">
-          {answer}
-        </p>
+        <div
+          ref={contentRef}
+          className="px-4 overflow-hidden transition-all duration-500 ease-in-out text-[#5a3a24]"
+          id={`faq-content-${index}`}
+          role="region"
+          aria-labelledby={`faq-header-${index}`}
+          style={{
+            maxHeight: isOpen ? contentRef.current?.scrollHeight : 0,
+            opacity: isOpen ? 1 : 0,
+          }}
+        >
+          <p className="py-2 text-sm text-walnut-brown/80 font-manrope ">
+            {answer}
+          </p>
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 FaqItem.displayName = "FaqItem";
 
