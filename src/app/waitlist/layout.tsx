0ab226fa6071d546f0b6cde668615e4acf638bb0
Chiamaka <lillyannmaka@gmail.com>
2025-10-01 11:11:19 +0100

    chore: adjusted the padding of the app name

--- src/app/waitlist/layout.tsx
+++ src/app/waitlist/layout.tsx
@@ -3,26 +3,26 @@ import { BrandLogo } from "@/components/ui";
 import { ReactNode } from "react";
 
 export default function WaitlistLayout({ children }: { children: ReactNode }) {
-    return (
-        <div className="flex flex-col min-h-screen px-8">
-            {/* Logo and App name */}
-            <div className="flex items-center pt-7">
-                <div className="-ml-4">
-                    <BrandLogo />
-                </div>
-                <Link
-                    href="/"
-                    className="text-walnut-brown font-sora text-2xl font-semibold tracking-tight -ml-2 pt-1"
-                    title="Go to the homepage"
-                    aria-label="Go to the homepage"
-                >
-                    <span>Focus</span>
-                    <span className="text-accent-button">Pond</span>
-                </Link>
-            </div>
-            <main className="flex flex-1 items-center justify-center">
-                <div className="w-full max-w-lg">{children}</div>
-            </main>
+  return (
+    <div className="flex flex-col min-h-screen px-8">
+      {/* Logo and App name */}
+      <div className="flex items-center pt-4">
+        <div className="-ml-4">
+          <BrandLogo />
         </div>
-    );
+        <Link
+          href="/"
+          className="text-walnut-brown font-sora text-2xl font-semibold tracking-tight -ml-2 pt-1"
+          title="Go to the homepage"
+          aria-label="Go to the homepage"
+        >
+          <span>Focus</span>
+          <span className="text-accent-button">Pond</span>
+        </Link>
+      </div>
+      <main className="flex flex-1 items-center justify-center">
+        <div className="w-full max-w-lg">{children}</div>
+      </main>
+    </div>
+  );
 }
--- src/app/waitlist/page.tsx
+++ src/app/waitlist/page.tsx
@@ -13,28 +13,41 @@ export default function WaitlistPage() {
     console.log("joining waitlist", email);
     // hook into backend or email service
   };
-  return (<div>
-    <div className="max-w-xs mx-auto">
-      <div className="text-center mb-6">
-        <Badge title="Join the Waitlist" />
-        <h1 className="text-3xl font-bold font-sora">Focus deeply. Rest intentionally.</h1>
-        <p className="my-3 font-manrope text-sm">Be the first to experience FocusPond when we launch.</p>
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="text-xs flex w-full rounded-full overflow-hidden border border-light-background/40 bg-secondary-accent/10 font-sora">
-          <input type="email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} required className="flex-1 px-4 py-3 text-gray-700 focus:outline-none" />
-          <button type="submit" className="cursor-pointer bg-accent-button px-4 py-2.5 rounded-full m-2 text-white relative overflow-hidden font-medium group ring-4 ring-base-background shadow"> <span className="relative z-10">Join Waitlist</span>
-            <span className="absolute inset-0 animate-shimmer bg-gradient-to-r from-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" /></button>
+  return (
+    <div>
+      <div className="max-w-xs mx-auto">
+        <div className="text-center mb-6">
+          <Badge title="Join the Waitlist" />
+          <h1 className="text-3xl font-bold font-sora">
+            Focus deeply. Rest intentionally.
+          </h1>
+          <p className="my-3 font-manrope text-sm">
+            Be the first to experience FocusPond when we launch.
+          </p>
         </div>
-      </form>
-    </div>
-    <div className="mt-20 grid sm:grid-cols-3 gap-6">
-      {waitlist.map(
-        (
-          { Icon, benefit },
-          index: number
-        ) => {
-
+        <form onSubmit={handleSubmit}>
+          <div className="text-xs flex w-full rounded-full overflow-hidden border border-light-background/40 bg-secondary-accent/10 font-sora">
+            <input
+              type="email"
+              placeholder="Your Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="flex-1 px-4 py-3 text-gray-700 focus:outline-none"
+            />
+            <button
+              type="submit"
+              className="cursor-pointer bg-accent-button px-4 py-2.5 rounded-full m-2 text-white relative overflow-hidden font-medium group ring-4 ring-base-background shadow"
+            >
+              {" "}
+              <span className="relative z-10">Join Waitlist</span>
+              <span className="absolute inset-0 animate-shimmer bg-gradient-to-r from-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="mt-20 grid sm:grid-cols-3 gap-6">
+        {waitlist.map(({ Icon, benefit }, index: number) => {
           return (
             <Card
               key={index}
@@ -44,7 +57,11 @@ export default function WaitlistPage() {
                 "border border-light-background/40 relative hover:shadow-lg bg-white/80"
               )}
             >
-              <div className={"absolute -top-4 left-1/2 -translate-x-1/2 rounded-full bg-accent-button p-3 ring-4 ring-base-background shadow"}>
+              <div
+                className={
+                  "absolute -top-4 left-1/2 -translate-x-1/2 rounded-full bg-accent-button p-3 ring-4 ring-base-background shadow"
+                }
+              >
                 <Icon className={clsx("w-3 h-3 text-white")} />
               </div>
               <p className="mt-3 px-3 font-bold text-sm font-manrope leading-relaxed text-center">
@@ -52,8 +69,8 @@ export default function WaitlistPage() {
               </p>
             </Card>
           );
-        }
-      )}
+        })}
+      </div>
     </div>
-  </div>);
+  );
 }
